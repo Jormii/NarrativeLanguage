ea265fefc6fb82c51b6717564d2070ae937594b0
Jorge <lopeznatalj@gmail.com>
2022-03-04 19:43:12 +0100

    Type checking and macro solving

---
+++ VirtualMachine/variables.py
@@ -0,0 +1,71 @@
+from enum import Enum, auto
+
+
+class VariableType(Enum):
+    INT = auto()
+    FLOAT = auto()
+    STRING = auto()
+
+
+class Identifier:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, o):
+        if not isinstance(o, Identifier):
+            return False
+
+        return self.name == o.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __repr__(self):
+        return self.name
+
+
+class Value:
+
+    def __init__(self, variable_type, literal):
+        self.variable_type = variable_type
+        self.literal = literal
+
+    def __repr__(self):
+        return "({}) {}".format(self.variable_type.name, self.literal)
+
+    @staticmethod
+    def congruent(value_a, value_b):
+        return value_a.variable_type == value_b.variable_type
+
+
+class Variable:
+
+    def __init__(self, identifier, value, index):
+        self.identifier = identifier
+        self.value = value
+        self.index = index
+
+    def __repr__(self):
+        return "{} = {}".format(self.identifier, self.value)
+
+
+class Variables:
+
+    def __init__(self):
+        self._in_order = []
+        self._index_mapping = {}
+
+    def defined(self, identifier):
+        return identifier in self._index_mapping
+
+    def define(self, identifier, value):
+        index = len(self._in_order)
+        variable = Variable(identifier, value, index)
+
+        self._in_order.append(variable)
+        self._index_mapping[identifier] = index
+
+    def read(self, identifier):
+        index = self._index_mapping[identifier]
+        return self._in_order[index]
--- main.py
+++ main.py
@@ -2,23 +2,37 @@ from NarrativeLanguage import scanner, parser
 from NarrativeLanguage.formatter import FORMATTER
 from NarrativeLanguage.interpreter import INTERPRETER
 
+from VirtualMachine.type_checker import TypeChecker
+
+DEBUG = False
+
 
 def main():
     path = "./example.txt"
     with open(path, "r") as f:
         source_code = f.read()
+
+        # Scan tokens
         scan = scanner.Scanner(source_code)
         scan.scan()
-        for token in scan.tokens:
-            print(token)
 
-        print(100 * "-")
+        if DEBUG:
+            for token in scan.tokens:
+                print(token)
+            print(100 * "-")
 
+        # Parse tokens
         parse = parser.Parser(scan.tokens)
         parse.parse()
-        for stmt in parse.statements:
-            print("> {}".format(FORMATTER.visit(stmt)))
-            print(INTERPRETER.visit(stmt))
+
+        if DEBUG:
+            for stmt in parse.statements:
+                print("> {}".format(FORMATTER.visit(stmt)))
+                print(INTERPRETER.visit(stmt))
+
+        # Check types and resolve macros
+        checker = TypeChecker(parse.statements)
+        checker.check()
 
 
 if __name__ == "__main__":
