9eecb845fcbd02261fd2d4da400109c1a68d171e
Jorge <lopeznatalj@gmail.com>
2022-03-20 12:48:13 +0100

    Macros are declared and replaced sequentially

--- NarrativeLanguage/macro_substitution.py
+++ NarrativeLanguage/macro_substitution.py
@@ -1,17 +1,9 @@
 from NarrativeLanguage.scanner import StringTraversal
 
 
-class Macro:
-
-    def __init__(self, text, start, end):
-        self.text = text
-        self.start = start
-        self.end = end
-
-
 def replace_macros(string):
     macros = {}
-    in_order = []
+    control_characters_indices = set()
 
     # Find macro declarations
     traversal = StringTraversal(string)
@@ -21,60 +13,91 @@ def replace_macros(string):
             break
 
         macro_start = traversal._pos
-        assert traversal.get_and_advance() == '#', "Expected '#'"
+        assert traversal.get() == '#', "Expected '#'"
 
+        # Skip control characters found
+        if macro_start in control_characters_indices:
+            traversal.get_and_advance()
+            continue
+
+        # Store control character
+        prev_char = None if traversal._pos == 0 else \
+            traversal.string[traversal._pos - 1]
+        if prev_char == '\\':
+            traversal.string = traversal.string[:macro_start - 1] + \
+                traversal.string[macro_start:]
+            control_characters_indices.add(macro_start - 1)
+            traversal._pos = 0  # Reset pointer
+            continue
+
+        # Extract identifier
+        traversal.get_and_advance()  # Consume '#'
+
+        assert _is_identifier_initial(traversal.get_and_advance()), \
+            "Identifier expected to start with a letter or '_'"
         while _is_identifier(traversal.get()):
             traversal.get_and_advance()
+
         identifier = traversal.string[macro_start + 1:traversal._pos]
 
         # Declarations are followed by a '='
         _skip_whitespaces(traversal)
-        if traversal.get() != '=':
-            continue
+        if traversal.get() == '=':
+            _declare_macro_and_remove(
+                macros, traversal, macro_start, identifier)
+        else:
+            _replace_macro(macros, traversal, macro_start, identifier)
 
-        traversal.get_and_advance()     # Consume '='
+        # Reset traversal pointer
+        traversal._pos = 0
 
-        # Get text
-        # Make sure the first character is a '('
-        _skip_whitespaces(traversal)
-        assert traversal.get_and_advance() == '(', "Expected '('"
+    return traversal.string
 
-        macro_decl_start = traversal._pos
-        parenthesis_depth = 1
-        while parenthesis_depth != 0 and not traversal.at_end():
-            c = traversal.get_and_advance()
-            if c == '(':
-                parenthesis_depth += 1
-            elif c == ')':
-                parenthesis_depth -= 1
 
-        macro_decl_end = traversal._pos
-        text = traversal.string[macro_decl_start:macro_decl_end - 1]
+def _declare_macro_and_remove(macros, traversal, macro_start, identifier):
+    # FORMAT
+    # #IDENTIFIER = (TEXT)
 
-        # Update variables
-        assert identifier not in macros, "Macro '{}' already defined"
+    assert identifier not in macros, \
+        "Macro '{}' already defined".format(identifier)
 
-        macros[identifier] = Macro(text, macro_start, macro_decl_end)
-        in_order.append(identifier)
+    # Check initial characters
+    _skip_whitespaces(traversal)
+    assert traversal.get_and_advance() == '=', "Expected '='"
 
-    # Remove macros from text
-    replaced = str(string)
-    for identifier in reversed(in_order):
-        macro = macros[identifier]
-        replaced = replaced[:macro.start] + replaced[macro.end:]
+    _skip_whitespaces(traversal)
+    assert traversal.get_and_advance() == '(', "Expected '('"
 
-    # Replace in text
-    for identifier, macro in macros.items():
-        str_to_find = "#{}".format(identifier)
-        length = len(str_to_find)
+    # Extract macro text
+    parenthesis_depth = 1
+    text_start_index = traversal._pos
+    while parenthesis_depth != 0 and not traversal.at_end():
+        c = traversal.get_and_advance()
+        if c == '(':
+            parenthesis_depth += 1
+        elif c == ')':
+            parenthesis_depth -= 1
 
-        start_index = replaced.find(str_to_find)
-        while start_index != -1:
-            replaced = replaced[:start_index] + \
-                macro.text + replaced[start_index + length:]
-            start_index = replaced.find(str_to_find)
+    assert parenthesis_depth == 0, \
+        "Found EOS while parsing macro '{}' declaration".format(identifier)
 
-    return replaced
+    macro_text = traversal.string[text_start_index: traversal._pos - 1]
+    macros[identifier] = macro_text
+
+    # Remove macro from string
+    traversal.string = traversal.string[:macro_start] + \
+        traversal.string[traversal._pos:]
+
+
+def _replace_macro(macros, traversal, macro_start, identifier):
+    # FORMAT
+    # #IDENTIFIER
+
+    assert identifier in macros, "Unknown macro '{}'".format(identifier)
+
+    macro_text = macros[identifier]
+    traversal.string = traversal.string[:macro_start] + macro_text + \
+        traversal.string[macro_start + len(identifier) + 1:]
 
 
 def _skip_to_pound(traversal):
@@ -87,8 +110,12 @@ def _skip_whitespaces(traversal):
         traversal.get_and_advance()
 
 
-def _is_identifier(char):
+def _is_identifier_initial(char):
     if char == StringTraversal.EOS:
         return False
 
-    return char.isdigit() or char.isalpha() or char == '_'
+    return char.isalpha() or char == '_'
+
+
+def _is_identifier(char):
+    return _is_identifier_initial(char) or char.isdigit()
